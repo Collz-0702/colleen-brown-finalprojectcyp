1ee454437b16a1005b29a0956ad31cf56a8a8347
Colleen <colleen.williams1905@gmail.com>
2022-12-16 23:24:24 -0500

    first commit

---
+++ cypress.config.js
@@ -0,0 +1,19 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: 'https://ui-automation-camp.vercel.app/',
+    specPattern: 'cypress/test-files/**/*.cy.js',
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/reports',
+      overwrite: false,
+      html: false,
+      json: true
+    },
+    chromeWebSecurity: false
+  },
+});
